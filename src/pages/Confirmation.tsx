10f2af7f4ac35e744dcaccdff0802b8de35bd153
Genivaldo <gscmail@gmail.com>
2021-04-25 22:25:29 -0300

    feat: Confirmation

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -18,57 +18,26 @@ import { Button } from '../components/Button';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
-export function UserIdentification() {
+export function Confirmation() {
 
-    const [isFocused, setIsFocused] = useState(false);
-    const [isFilled, setIsFilled] = useState(false);
-    const [name, setName] = useState<string>();
-
-    function handleInputBlur() {
-        setIsFocused(false);
-        setIsFilled(!!name);
-    }
-
-    function handleInputFocus() {
-        setIsFocused(true);
-    }
-
-    function handleInputChange(value: string) {
-        setIsFilled(!!value);
-        setName(name);
-
-    }
 
     return (
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView
                 style={styles.container}
-                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
                 <View style={styles.content}>
                     <View style={styles.form}>
 
                         <View style={styles.header}>
                             <Text style={styles.emoji}>
-                                {isFilled ? '😄️' : '😀️'}
+                                😄️
                             </Text>
 
                             <Text style={styles.title}>
-                                Como podemos {'\n'}
-                                chamar você?
+                                Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
                             </Text>
                         </View>
-
-                        <TextInput
-                            style={[
-                                styles.input,
-                                (isFocused || isFilled) && { borderColor: colors.green }
-                            ]}
-                            placeholder="Digite um nome"
-                            onFocus={handleInputFocus}
-                            onBlur={handleInputBlur}
-                            onChangeText={handleInputChange}
-                        />
 
                         <View style={styles.footer}>
                             <Button title="Confirmar" />
