7aa659519a44c5d00d9d9dcae0a7e445d5c4d2e9
Genivaldo <gscmail@gmail.com>
2021-05-08 08:34:34 -0300

    feat: listagem de ambientes

---
+++ src/components/PlantCardPrimary.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+
+
+export function PlantCardPrimary() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    text: {
+
+    }
+})
