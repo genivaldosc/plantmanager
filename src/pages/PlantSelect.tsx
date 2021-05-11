9f8270158724bab9f20df05be856d4a0514cb406
Genivaldo <gscmail@gmail.com>
2021-05-10 21:57:02 -0300

    fix: scroll infinito

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -3,7 +3,8 @@ import {
     View,
     Text,
     StyleSheet,
-    FlatList
+    FlatList,
+    ActivityIndicator
 } from 'react-native';
 import { EnviromentButton } from '../components/EnviromentButton';
 import { Header } from '../components/Header';
@@ -35,28 +36,37 @@ interface PlantProps {
 
 export function PlantSelect() {
 
-    const [enviroments, setEnviroments] = useState<EnviromentProps[]>();
-    const [plants, setPlants] = useState<PlantProps[]>();
-    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>();
+    const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
+    const [plants, setPlants] = useState<PlantProps[]>([])
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
     const [enviromentSelected, setEnviromentSelected] = useState('all');
     const [loading, setLoading] = useState(true);
 
     const [page, setPage] = useState(1);
     const [loadingMore, setLoadingMore] = useState(true);
-    const [loadedAll, setLoadedAll] = useState(false);
+    // const [loadedAll, setLoadedAll] = useState(false);
 
 
-    async function fetchEnviroment() {
+
+
+    async function fetchPlants() {
         const { data } = await api
-            .get('plants_environments?_sort=title&_order=asc');
+            .get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
 
-        setEnviroments([
-            {
-                key: 'all',
-                title: 'Todos'
-            },
-            ...data
-        ]);
+        if (!data) {
+            return setLoading(true);
+        }
+
+        if (page > 1) {
+            setPlants(oldValue => [...oldValue, ...data])
+            setFilteredPlants(oldValue => [...oldValue, ...data])
+        } else {
+            setPlants(data);
+            setFilteredPlants(data);
+        }
+
+        setLoading(false);
+        setLoadingMore(false);
     }
 
     function handleEnviromentSelected(enviroment: string) {
@@ -80,37 +90,31 @@ export function PlantSelect() {
 
         setLoadingMore(true);
         setPage(oldValue => oldValue + 1);
-        fetchEnviroment();
+        fetchPlants();
     }
 
 
     useEffect(() => {
+
+        async function fetchEnviroment() {
+            const { data } = await api
+                .get('plants_environments?_sort=title&_order=asc');
+
+            setEnviroments([
+                {
+                    key: 'all',
+                    title: 'Todos'
+                },
+                ...data
+            ]);
+        }
 
         fetchEnviroment();
 
     }, []);
 
     useEffect(() => {
-        async function fetchPlants() {
-            const { data } = await api
-                .get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
-
-            if (!data) {
-                return setLoading(true);
-            }
-
-            if (page > 1) {
-                setPlants(oldValue => [...oldValue, ...data]);
-                setFilteredPlants(oldValue => [...oldValue, ...data]);
-            }
-
-            // setPlants(data);
-            // setFilteredPlants(data);
-            setLoading(false);
-        }
-
         fetchPlants();
-
     }, []);
 
     if (loading) {
@@ -158,6 +162,14 @@ export function PlantSelect() {
                     showsVerticalScrollIndicator={false}
                     numColumns={2}
                     onEndReachedThreshold={0.1}
+                    onEndReached={({ distanceFromEnd }) =>
+                        handleFetchMore(distanceFromEnd)
+                    }
+                    ListEmptyComponent={
+                        loadingMore
+                            ? <ActivityIndicator color={colors.green} />
+                            : <></>
+                    }
 
                 />
 
